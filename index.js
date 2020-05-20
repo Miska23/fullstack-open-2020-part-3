86f8169d0f2dd6f329b0fa2a8ece4dc741c692c4
Miska23 <miskalinden@gmail.com>
2020-05-20 16:33:42 +0300

    finished routes and added validation to Mongoose

--- index.js
+++ index.js
@@ -67,52 +67,23 @@ app.get('/api/persons/:id', (req, res, next) => {
   .catch(error => next(error))
 })
 
-//? tälle myös next erroreita varten?
-app.post('/api/persons/', (req, res) => {
+app.post('/api/persons/', (req, res, next) => {
 
   const body = req.body;
-
-  console.log('from app.post / 1, body is', body);
-
-  if (!body.name) {
-    return res.status(400).json({
-      error: 'name is missing' //! ei tulostu konsoliin!
-    })
-  }
-
-  if (!body.number) {
-    return res.status(400).json({
-      error: 'number is missing'//! ei tulostu konsoliin!
-    })
-  }
-
-  /*
-  //! jos ei löydy, arvoksi tulee undefined joka on falsy
-  //! jos löytyy, arvoksi tulee löydetty olio joka on aina truthy
-  const nameAlreadyExists = persons.find(person => person.name === body.name)
-
-  if (nameAlreadyExists) {
-    return res.status(201).json({
-      error: `name ${body.name} is already in the phonebook`
-    })
-  }
- */
-
-   //! Kurssimateriaalin malli:
-
-   const person = new Person({
+  
+  const person = new Person({
     name: body.name,
     number: body.number
   })
 
-  console.log('from app.post / 2, Person is', person);
-
-  person.save().then(savedPerson => {
+  person.save()
+    .then(savedPerson => {
       console.log('from app.post / 4 / .then started')
       res.json(savedPerson)
       console.log('from app.post / 4 / .then resolved, savedPerson is: ', savedPerson)
     })
-})
+    .catch(error => next(error)) //! error on koko virheolio
+  })
 
 //! fronttia voisi muokata mallivastauksen mukaan siten että
 //! nimi tulee olem. olevasta henkilöstä eikä kentästä
@@ -127,7 +98,7 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, person, { runValidators: true, context: 'query', new: true })
     .then(updatedPerson => {
       console.log('from updateOne / 2, updatedPerson is: ', updatedPerson)
       res.json(updatedPerson)
@@ -140,7 +111,11 @@ app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       console.log('from deleteOne / 2 / .then, result is: ', result)
-      res.status(204).end()
+      if (result) {
+        res.status(204).end()
+      } else { //! tämä vie frontissa .catchiin
+        res.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
@@ -157,10 +132,12 @@ const errorHandler = (err, req, res, next) => {
   console.error(err.message) 
 
   if (err.name === 'CastError') { 
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).send({ err: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ err: err.message })
   }
   //! jos virhe ei ole CastError, virhe menee Expressin oletusvirheidenkäsittelijälle
-  next(error)
+  next(err)
 }
 
 app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 //! poistaa findOneAndUpdate()`ja `findOneAndDelete() -metodeihin liittyvään varoituksen:
 mongoose.set('useFindAndModify', false) 
 
 const url = process.env.MONGODB_URI;
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -14,9 +15,11 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number
+  name: { type: String, minlength: 3, required: true, unique: true},
+  number: { type: String, minlength: 8, required: true }
 })
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
